ddb68c156bace2602d3529082797d6acb1e2e6af
GoldenEdit <sammyhmatthews@gmail.com>
2023-05-18 12:03:46 +0100

    highlight when papers have been clicked on and add clear button

--- script.js
+++ script.js
@@ -1,57 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href^="#"]');
-    for (var i = 0; i < links.length; i++) {
-      links[i].addEventListener('click', smoothScroll);
-    }
-    
-    function smoothScroll(event) {
-      event.preventDefault();
-      var targetId = this.getAttribute('href');
-      var targetElement = document.querySelector(targetId);
-      if (targetElement) {
-        var targetPosition = targetElement.offsetTop;
-        window.scrollTo({
-          top: targetPosition,
-          behavior: 'smooth'
-        });
-      }
-    }
-  });
+    var links = document.querySelectorAll('a[href$=".pdf"]');
   
-  document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a[href^="#"]');
-    
     // Check if the user has any saved preferences
     var savedPreferences = localStorage.getItem('examResourcesPreferences');
     if (savedPreferences) {
       var preferences = JSON.parse(savedPreferences);
       applyPreferences(preferences);
     }
-    
+  
     for (var i = 0; i < links.length; i++) {
       links[i].addEventListener('click', handleLinkClick);
     }
-    
+  
     function handleLinkClick(event) {
-      event.preventDefault();
       var targetId = this.getAttribute('href');
-      var targetElement = document.querySelector(targetId);
-      if (targetElement) {
-        var targetPosition = targetElement.offsetTop;
-        window.scrollTo({
-          top: targetPosition,
-          behavior: 'smooth'
-        });
   
-        // Save the user's preference
-        var preferences = getPreferences();
-        preferences[targetId] = true;
-        savePreferences(preferences);
+      // Save the user's preference
+      var preferences = getPreferences();
+      preferences[targetId] = true;
+      savePreferences(preferences);
   
-        applyPreferences(preferences);
-      }
+      applyPreferences(preferences);
     }
-    
+  
     function getPreferences() {
       var preferences = localStorage.getItem('examResourcesPreferences');
       if (preferences) {
@@ -59,11 +30,11 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       return {};
     }
-    
+  
     function savePreferences(preferences) {
       localStorage.setItem('examResourcesPreferences', JSON.stringify(preferences));
     }
-    
+  
     function applyPreferences(preferences) {
       for (var i = 0; i < links.length; i++) {
         var targetId = links[i].getAttribute('href');
@@ -74,5 +45,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       }
     }
+    var clearButton = document.getElementById('clearButton');
+    clearButton.addEventListener('click', handleClearClick);
+  
+    function handleClearClick() {
+      var preferences = getPreferences();
+      preferences = {};
+      savePreferences(preferences);
+      applyPreferences(preferences);
+    }
+    
   });
+
+
+  
   
